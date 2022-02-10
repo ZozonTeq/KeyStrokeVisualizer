d4e9ec0c5935d844816f9108f04dfdea88237984
zo <77091655+ZozonTeq@users.noreply.github.com>
2022-02-10 22:04:49 +0900

    Add files via upload

---
+++ KeyStrokeVisualizer/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace KeyStrokeVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.ricktxt_log = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_key_per_sec = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.LightCoral;
+            this.btn_exit.FlatAppearance.BorderColor = System.Drawing.Color.LightCoral;
+            this.btn_exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_exit.ForeColor = System.Drawing.Color.Snow;
+            this.btn_exit.Location = new System.Drawing.Point(239, 0);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(33, 29);
+            this.btn_exit.TabIndex = 1;
+            this.btn_exit.Text = "×";
+            this.btn_exit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // ricktxt_log
+            // 
+            this.ricktxt_log.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ricktxt_log.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ricktxt_log.Font = new System.Drawing.Font("ノスタルドット（M+）", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ricktxt_log.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ricktxt_log.Location = new System.Drawing.Point(14, 12);
+            this.ricktxt_log.Multiline = false;
+            this.ricktxt_log.Name = "ricktxt_log";
+            this.ricktxt_log.ReadOnly = true;
+            this.ricktxt_log.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ricktxt_log.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.ricktxt_log.Size = new System.Drawing.Size(206, 62);
+            this.ricktxt_log.TabIndex = 3;
+            this.ricktxt_log.Text = "";
+            this.ricktxt_log.TextChanged += new System.EventHandler(this.ricktxt_log_TextChanged);
+            this.ricktxt_log.Enter += new System.EventHandler(this.ricktxt_log_Enter);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("ノスタルドット（M+）", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(10, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 23);
+            this.label1.TabIndex = 4;
+            // 
+            // label_key_per_sec
+            // 
+            this.label_key_per_sec.AutoSize = true;
+            this.label_key_per_sec.Font = new System.Drawing.Font("ノスタルドット（M+）", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_key_per_sec.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label_key_per_sec.Location = new System.Drawing.Point(195, 77);
+            this.label_key_per_sec.Name = "label_key_per_sec";
+            this.label_key_per_sec.Size = new System.Drawing.Size(59, 21);
+            this.label_key_per_sec.TabIndex = 5;
+            this.label_key_per_sec.Text = "0 KPS";
+            this.label_key_per_sec.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(266, 134);
+            this.ControlBox = false;
+            this.Controls.Add(this.label_key_per_sec);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ricktxt_log);
+            this.Controls.Add(this.btn_exit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.RichTextBox ricktxt_log;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_key_per_sec;
+    }
+}
+
---
+++ KeyStrokeVisualizer/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyStrokeVisualizer
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int GetAsyncKeyState(int vKey);
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //--------------------------------------
+            int radius = 20;
+            int diameter = radius * 2;
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddPie(0, 0, diameter, diameter, 180, 90);
+            gp.AddPie(this.Width - diameter, 0, diameter, diameter, 270, 90);
+            gp.AddPie(0, this.Height - diameter, diameter, diameter, 90, 90);
+            gp.AddPie(this.Width - diameter, this.Height - diameter, diameter, diameter, 0, 90);
+            gp.AddRectangle(new Rectangle(radius, 0, this.Width - diameter, this.Height));
+            gp.AddRectangle(new Rectangle(0, radius, radius, this.Height - diameter));
+            gp.AddRectangle(new Rectangle(this.Width - radius, radius, radius, this.Height - diameter));
+            this.Region = new Region(gp);
+            //--------------------------------------
+           
+            btn_exit.FlatAppearance.BorderSize = 0;
+            this.btn_exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            btn_exit.Visible = false;
+            //label1.BackColor = Color.Transparent;
+            ricktxt_log.Enabled = false;
+            ricktxt_log.Enabled = true;
+            ricktxt_log.Parent = this;
+            ricktxt_log.BackColor =this.BackColor;
+            label_key_per_sec.ForeColor = ricktxt_log.ForeColor;
+        }
+        private void btn_exit_Click(object sender, EventArgs e)=> this.Close();
+
+        private void ricktxt_log_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void AddKeyLog(String s)
+        {
+            ricktxt_log.SelectionLength = 0;
+            ricktxt_log.SelectionStart = ricktxt_log.TextLength;
+
+            ricktxt_log.SelectionColor = Color.White;
+            Font baseFont = ricktxt_log.SelectionFont;
+            Font fnt = new Font(baseFont.FontFamily,
+                baseFont.Size,
+                baseFont.Style | FontStyle.Bold);
+            ricktxt_log.SelectionFont = fnt;
+            ricktxt_log.SelectedText = s;
+            ricktxt_log.SelectionAlignment = HorizontalAlignment.Center;
+
+            label1.Text = s + " " + label1.Text;
+
+            baseFont.Dispose();
+            fnt.Dispose();
+        }
+        string sublog = "";
+        public void SetKeyLog(String s)
+        {
+
+            ricktxt_log.Text = "";
+            AddKeyLog(s);
+        }
+        int tickc = 0;
+        int tick_kb = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tickc++;
+            bool[] gKeys = new bool[257];
+            for (int i = 0; i <= 256; i++) gKeys[i] = false;
+            for (int i = 1; i <= 256; i++)
+            {
+                int rtn = GetAsyncKeyState(i);
+                if ((rtn & 1) != 0)
+                {
+                    Keys k = (Keys)i;
+                    gKeys[i] = true;
+                    tick_kb++;
+                }
+            }
+            string r = String.Empty;
+            for(int i = 1; i <= 256; i++)
+            {
+                if (gKeys[i])
+                {
+                    string kk = "";
+                    Keys k = (Keys)i;
+
+                    String[] blockKs ={ "ControlKey","LButton","RButton" ,"Menu"};//表示しない　キー
+                    kk = k.ToString();
+                    bool b = false;
+                    for(int j = 0;j < blockKs.Length;j++)
+                    {
+                        if (k.ToString() == blockKs[j]) b = true;
+                    }
+                    if (!b)
+                    {
+                        if (kk == "LWin"||kk=="RWin")
+                        {
+                            kk = "Win";
+                        }
+                        else if (kk == "LControlKey"||kk== "RControlKey")
+                        {
+                            kk = "Ctrl";
+                        }
+                        else if (kk == "LMenu"||kk== "RMenu")
+                        {
+                            kk = "Alt";
+                        }
+                        r += kk + "+";
+                    }
+
+                }   
+            }
+            if (r != String.Empty)
+            {
+                r = r.Substring(0, r.Length - 1);
+                SetKeyLog(r);
+
+            }
+            if (tickc == 10)
+            {
+                tickc = 0;
+                label_key_per_sec.Text =(tick_kb * timer1.Interval*0.001*10).ToString()+" KPS";
+                tick_kb = 0;
+            }
+        }
+
+        private Point mousePoint;
+        private void Form1_MouseDown(object sender,MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                mousePoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void Form1_MouseMove(object sender,
+            System.Windows.Forms.MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                this.Left += e.X - mousePoint.X;
+                this.Top += e.Y - mousePoint.Y;
+            }
+        }
+
+        private void ricktxt_log_Enter(object sender, EventArgs e)
+        {
+            label1.Focus();
+        }
+    }
+}
